4ff18474ca004e09d7273da75181e24ca3fe9713
Pranay Shah <greatpranayshah@gmail.com>
2016-04-12 22:57:43 +0530

    More internationalization again

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
@@ -119,7 +119,7 @@ public class MyStocksActivity extends AppCompatActivity implements LoaderManager
                       new String[] { input.toString() }, null);
                   if (c!= null && c.getCount() != 0) {
                     Toast toast =
-                        Toast.makeText(MyStocksActivity.this, R.string.stock_saved,
+                        Toast.makeText(MyStocksActivity.this, getString(R.string.stock_saved),
                             Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.CENTER, Gravity.CENTER, 0);
                     toast.show();
